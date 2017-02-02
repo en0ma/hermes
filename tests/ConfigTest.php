4b7e83a7c4014b8b43bff52f01a3830c45fae919
Ezinwa Okpoechi <brainmaestro@outlook.com>
2017-02-02 10:03:56 +0100

    Add ability to retrieve multiple channels from the config

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -17,6 +17,7 @@ class ConfigTest extends PHPUnit_Framework_TestCase
             'channels' => [
                 'test1' => ['path' => 'test.py'],
                 'test2' => ['path' => 'test.php'],
+                'test3' => ['path' => 'test.rb'],
             ]
         ];
         self::$config = new Config(self::$configData);
@@ -28,15 +29,25 @@ class ConfigTest extends PHPUnit_Framework_TestCase
     public function it_should_return_the_correct_channel_using_an_alias()
     {
         $alias = 'test1';
-        $this->assertEquals(self::$configData['channels'][$alias], self::$config->getChannel($alias));
+        $this->assertEquals([self::$configData['channels'][$alias]], self::$config->getChannels($alias));
     }
 
     /**
      * @test
      */
-    public function it_should_return_null_with_an_incorrect_alias()
+    public function it_should_return_the_correct_channels_using_several_aliases()
+    {
+        $aliases = ['test1', 'test3'];
+        $expectedChannels = [self::$configData['channels'][$aliases[0]], self::$configData['channels'][$aliases[1]]];
+        $this->assertEquals($expectedChannels, self::$config->getChannels($aliases));
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_return_an_empty_array_with_an_incorrect_alias()
     {
         $alias = 'channel_does_not_exist';
-        $this->assertNull(self::$config->getChannel($alias));
+        $this->assertEmpty(self::$config->getChannels($alias));
     }
 }
