e42cbf7df7f1f63ab8fd53bd43988d44854c31b1
Lawrence Enehizena <lawrence.enehizena@irokopartners.com>
2017-02-02 17:51:13 +0100

    minor-cleanup

--- src/Formatter.php
+++ src/Formatter.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by BrainMaestro
- * Date: 01/02/2017
- * Time: 4:40 PM
- */
-
 namespace Lawstands\Hermes;
 
 interface Formatter
--- src/Formatter/JsonFormatter.php
+++ src/Formatter/JsonFormatter.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by BrainMaestro.
- * Date: 01/02/2017
- * Time: 4:43 PM
- */
-
 namespace Lawstands\Hermes\Formatter;
 
 use Lawstands\Hermes\Formatter;
@@ -19,12 +13,9 @@ class JsonFormatter implements Formatter
      */
     public function format($data)
     {
-        // return the data if it is a primitive type
-        // that does not require json encoding
         if (is_scalar($data)) {
             return (string) $data;
         }
-
         return json_encode($data);
     }
 }
